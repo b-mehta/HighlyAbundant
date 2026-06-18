a294bdd3072ee652db90ff76ef3bb82ec8b0e5bc
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-06-18 02:38:26 +0000

    Add per-n bench isolation files for n=8, 27, 64, 89, 125, 127

---
+++ HighlyAbundant/Bench/N008.lean
@@ -0,0 +1,9 @@
+import HighlyAbundant.SageKernel
+import HighlyAbundant.Bench.QuickRfl
+
+namespace Sage
+
+theorem stepK_lcm_8 :
+    stepK 840 searchFuel [(2880, 1, 0)] == some true := by quickRfl
+
+end Sage
---
+++ HighlyAbundant/Bench/N027.lean
@@ -0,0 +1,9 @@
+import HighlyAbundant.SageKernel
+import HighlyAbundant.Bench.QuickRfl
+
+namespace Sage
+
+theorem stepK_lcm_27 :
+    stepK 80313433200 searchFuel [(446371430400, 1, 0)] == some true := by quickRfl
+
+end Sage
