f12f7074a5f6b091890b1e4e3633ea1da1ab8700
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-07-06 16:44:05 +0000

    Set copyright year to 2026 across the development, add missing headers

--- HighlyAbundant/IsHA/SageKernel.lean
+++ HighlyAbundant/IsHA/SageKernel.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Bhavik Mehta. All rights reserved.
+Copyright (c) 2026 Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
