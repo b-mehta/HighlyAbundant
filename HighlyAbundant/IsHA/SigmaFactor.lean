e80cb18338a40cc0384adcf708585e1bd4199bbf
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-25 00:31:48 +0000

    Name the two duplicate-free facts for their lists

--- HighlyAbundant/IsHA/SigmaFactor.lean
+++ HighlyAbundant/IsHA/SigmaFactor.lean
@@ -87,14 +87,14 @@ theorem sigma_of_factorization {sL : ℕ} {F : List (ℕ × ℕ)} (hp : allCheck
     (hc : FactorChain F) (hsig : sigmaFactorK F = sL) :
     σ₁ (prodFactorK F) = sL := by
   have hpp := forall_prime_of_checkPrime hp
-  have hd : (F.map Prod.fst).Nodup := (hc.pairwise.imp Nat.ne_of_lt).map _ fun _ _ ↦ id
-  have hnd : F.Nodup := hd.of_map
+  have hfst : (F.map Prod.fst).Nodup := (hc.pairwise.imp Nat.ne_of_lt).map _ fun _ _ ↦ id
+  have hnd : F.Nodup := hfst.of_map
   have hcop : (F.toFinset : Set (ℕ × ℕ)).Pairwise
       fun a b ↦ Nat.Coprime (a.1 ^ a.2) (b.1 ^ b.2) := by
     intro a ha b hb hab
     simp only [List.coe_toFinset, Set.mem_ofPred_eq] at ha hb
     exact Nat.Coprime.pow _ _ ((Nat.coprime_primes (hpp a ha) (hpp b hb)).2
-      fun he ↦ hab (List.inj_on_of_nodup_map hd ha hb he))
+      fun he ↦ hab (List.inj_on_of_nodup_map hfst ha hb he))
   subst hsig
   rw [prodFactorK_eq, sigmaFactorK_eq, ← List.prod_toFinset _ hnd, ← List.prod_toFinset _ hnd,
     isMultiplicative_sigma.map_prod _ _ hcop]
